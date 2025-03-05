6c56d8adde87fb3ad298b9bc2686cad459d2d142
Esh <evargaspatrick@stetson.edu>
2025-03-05 13:56:38 -0500

    Fixed buttons/fonts
    Fixed buttons to have the appropriate colors and added the 'Inter' font to the project.

--- frontend/src/route/routes.jsx
+++ frontend/src/route/routes.jsx
@@ -1,30 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
+import RootLayout from "../layouts/RootLayout";
 import HomePage from "../pages/homepage";
 import SignupPage from "../pages/signup1";
 import Verify from "../pages/verify";
 import Login from "../pages/login";
-import Dashboard from "../pages/dashboard"; // Add this import
+import Dashboard from "../pages/dashboard";
 
 const router = createBrowserRouter([
   {
-    path: "/",    
-    element: <HomePage />
-  },
-  {
-    path:"/signup",
-    element: <SignupPage/>
-  },
-  {
-    path:"/verify",
-    element: <Verify/>
-  },
-  {
-    path:"/login",
-    element: <Login/>
-  },
-  {
-    path:"/dashboard", // Add the dashboard route
-    element: <Dashboard/>
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "signup", element: <SignupPage /> },
+      { path: "verify", element: <Verify /> },
+      { path: "login", element: <Login /> },
+      { path: "dashboard", element: <Dashboard /> }
+    ]
   }
 ]);
 
