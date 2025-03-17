cf41f4537c9eee1ab6064ea5ea23b2809c563d15
Meirzhan <meirzhansaparov@Meirzhans-MacBook-Pro.local>
2025-03-17 12:39:21 -0400

    got rid of google cloud variable

--- backend/cliquepay/models.py
+++ backend/cliquepay/models.py
@@ -106,3 +106,37 @@ class GroupMember(models.Model):
 
     def __str__(self):
         return f"{self.user.full_name} in {self.group.name}"
+    
+class Expense(models.Model):
+    id = models.CharField(max_length=128, primary_key=True, unique=True)
+    group_id = models.ForeignKey(
+        Group,
+        on_delete=models.CASCADE,
+        related_name='expenses'
+    )
+
+    friend_id = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='expenses'
+    )
+
+    paid_by = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='expenses_paid'
+    )
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    remaining_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(blank=True, null=True) # Optional deadline for payment
+    receipt_url = models.URLField(blank=True, null=True)
+
+    
+
+    class Meta:
+        db_table = 'expenses'
+
+    def __str__(self):
+        return f"{self.paid_by.full_name} paid ${self.amount} for {self.group.name}"
